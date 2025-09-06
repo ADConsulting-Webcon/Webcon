3ecd74f2db1833247eda541294b765e3689bc83a
AD Consulting <133123124+Ganitt@users.noreply.github.com>
2025-09-06 15:54:51 +0200

    Update Privileges.sql

--- Privileges.sql
+++ Privileges.sql
@@ -1,73 +1,135 @@
 -- INFO 
 -- Replace {User} user login with the COS_BPSID format.
 
+---------------------------------------------------
 -- Group information
-
-select 
-u.COS_BPSID as Login, 
-u.COS_DisplayName as Name,
-g.COS_DisplayName as GroupName,
-iif(g.COS_AccountType = 4, 'AD','BPS') as Type
-from [dbo].[CacheOrganizationStructure] u
-join [dbo].[CacheOrganizationStructureGroupRelations] gr
-on gr.COSGR_UserID = u.COS_ID
-join [dbo].[CacheOrganizationStructure] g
-on g.COS_ID = gr.COSGR_GroupID
-WHERE
-u.COS_IsActive = 1 and u.COS_AccountType = 1 and u.COS_BPSID = '{User}'
-order by Name
-
---Global privileges 
+---------------------------------------------------
 SELECT 
- [Name] as [Type of privileges],
-iif(SEC_USERGUID='{USER}','User','Group') as [User/Group],
-SEC_Username as [Name]
- FROM [dbo].[WFSecurities] 
- JOIN [dbo].[DicSecurityLevels] on SEC_LevelID = [TypeID]
- WHERE SEC_IsPermanent = 1 and  (SEC_USERGUID = '{USER}' or '{USER}' in (select u.COS_BpsID from [dbo].[CacheOrganizationStructure] u join [dbo].[CacheOrganizationStructureGroupRelations] gr on gr.COSGR_UserID = u.COS_ID join [dbo].[CacheOrganizationStructure] g on g.COS_ID = gr.COSGR_GroupID where g.COS_DisplayName = SEC_UserName ))
+    u.COS_BPSID       AS Login, 
+    u.COS_DisplayName AS Name,
+    g.COS_DisplayName AS GroupName,
+    IIF(g.COS_AccountType = 4, 'AD', 'BPS') AS Type
+FROM [dbo].[CacheOrganizationStructure] u
+JOIN [dbo].[CacheOrganizationStructureGroupRelations] gr
+    ON gr.COSGR_UserID = u.COS_ID
+JOIN [dbo].[CacheOrganizationStructure] g
+    ON g.COS_ID = gr.COSGR_GroupID
+WHERE u.COS_IsActive = 1
+  AND u.COS_AccountType = 1
+  AND u.COS_BPSID = '{User}'
+ORDER BY Name;
 
---Privileges per process
-
- SELECT 
-[Name] as [Type of privileges],
-DEF_Name as [Proces],
-iif(SEC_USERGUID='{User}','User','Group') as [User/Group],
-SEC_Username as [Name]
+---------------------------------------------------
+-- Global privileges 
+---------------------------------------------------
+SELECT 
+    [Name] AS [Type of privileges],
+    IIF(SEC_USERGUID = '{USER}', 'User', 'Group') AS [User/Group],
+    SEC_Username AS [Name]
 FROM [dbo].[WFSecurities] 
-JOIN [dbo].[DicSecurityLevels] on SEC_LevelID = [TypeID]
-JOIN [dbo].WFDefinitions on DEF_ID = SEC_DEFID
-WHERE  (SEC_USERGUID = '{User}' or '{User}' in (select u.COS_BpsID from [dbo].[CacheOrganizationStructure] u join [dbo].[CacheOrganizationStructureGroupRelations] gr on gr.COSGR_UserID = u.COS_ID join [dbo].[CacheOrganizationStructure] g on g.COS_ID = gr.COSGR_GroupID where g.COS_DisplayName = SEC_UserName ))
-and SEC_DEFID is not null
+JOIN [dbo].[DicSecurityLevels] 
+    ON SEC_LevelID = [TypeID]
+WHERE SEC_IsPermanent = 1
+  AND (
+        SEC_USERGUID = '{USER}' 
+        OR '{USER}' IN (
+            SELECT u.COS_BpsID
+            FROM [dbo].[CacheOrganizationStructure] u
+            JOIN [dbo].[CacheOrganizationStructureGroupRelations] gr
+                ON gr.COSGR_UserID = u.COS_ID
+            JOIN [dbo].[CacheOrganizationStructure] g
+                ON g.COS_ID = gr.COSGR_GroupID
+            WHERE g.COS_DisplayName = SEC_UserName
+        )
+      );
 
+---------------------------------------------------
+-- Privileges per process
+---------------------------------------------------
+SELECT 
+    [Name] AS [Type of privileges],
+    DEF_Name AS [Proces],
+    IIF(SEC_USERGUID = '{User}', 'User', 'Group') AS [User/Group],
+    SEC_Username AS [Name]
+FROM [dbo].[WFSecurities] 
+JOIN [dbo].[DicSecurityLevels] 
+    ON SEC_LevelID = [TypeID]
+JOIN [dbo].[WFDefinitions] 
+    ON DEF_ID = SEC_DEFID
+WHERE (
+        SEC_USERGUID = '{User}' 
+        OR '{User}' IN (
+            SELECT u.COS_BpsID
+            FROM [dbo].[CacheOrganizationStructure] u
+            JOIN [dbo].[CacheOrganizationStructureGroupRelations] gr
+                ON gr.COSGR_UserID = u.COS_ID
+            JOIN [dbo].[CacheOrganizationStructure] g
+                ON g.COS_ID = gr.COSGR_GroupID
+            WHERE g.COS_DisplayName = SEC_UserName
+        )
+      )
+  AND SEC_DEFID IS NOT NULL;
+
+---------------------------------------------------
 -- Privileges per Workflow/DocType
-
+---------------------------------------------------
 SELECT 
-[Name] as [Type of privileges],
-iif(SEC_USERGUID='{User}','User','Group') as [User/Group],
-SEC_Username as [Name],
-DEF_Name as Proces,
-WF_Name as Workflow,
-DTYPE_Name as DocType
-FROM [dbo].[WFSecurities] as Sec
-JOIN [dbo].[DicSecurityLevels] on SEC_LevelID = [TypeID]
-JOIN dbo.DocTypeAssocciations on SEC_ASSID = ASS_ID
-JOIN dbo.WorkFlows on WF_ID = ASS_WFID
-JOIN dbo.WFDefinitions on WF_WFDEFID = DEF_ID
-JOIN dbo.WFDocTypes on DTYPE_ID = ASS_DTYPEID
-WHERE  (SEC_USERGUID = '{User}' or '{User}' in (select u.COS_BpsID from [dbo].[CacheOrganizationStructure] u join [dbo].[CacheOrganizationStructureGroupRelations] gr on gr.COSGR_UserID = u.COS_ID join [dbo].[CacheOrganizationStructure] g on g.COS_ID = gr.COSGR_GroupID where g.COS_DisplayName = SEC_UserName ))
+    [Name] AS [Type of privileges],
+    IIF(SEC_USERGUID = '{User}', 'User', 'Group') AS [User/Group],
+    SEC_Username AS [Name],
+    DEF_Name AS Proces,
+    WF_Name AS Workflow,
+    DTYPE_Name AS DocType
+FROM [dbo].[WFSecurities] AS Sec
+JOIN [dbo].[DicSecurityLevels] 
+    ON SEC_LevelID = [TypeID]
+JOIN dbo.DocTypeAssocciations 
+    ON SEC_ASSID = ASS_ID
+JOIN dbo.WorkFlows 
+    ON WF_ID = ASS_WFID
+JOIN dbo.WFDefinitions 
+    ON WF_WFDEFID = DEF_ID
+JOIN dbo.WFDocTypes 
+    ON DTYPE_ID = ASS_DTYPEID
+WHERE (
+        SEC_USERGUID = '{User}' 
+        OR '{User}' IN (
+            SELECT u.COS_BpsID
+            FROM [dbo].[CacheOrganizationStructure] u
+            JOIN [dbo].[CacheOrganizationStructureGroupRelations] gr
+                ON gr.COSGR_UserID = u.COS_ID
+            JOIN [dbo].[CacheOrganizationStructure] g
+                ON g.COS_ID = gr.COSGR_GroupID
+            WHERE g.COS_DisplayName = SEC_UserName
+        )
+      );
 
+---------------------------------------------------
 -- Privileges per Elements
-
+---------------------------------------------------
 SELECT 
-[Name] as [Type of privileges],
-iif(SEC_USERGUID='{User}','User','Group') as [User/Group],
-SEC_Username as [Name],
-DEF_Name as Proces,
-WF_Name as Workflow,
-DTYPE_Name as DocType,
-WFD_Signature as Signature,
-WFD_ID as ID
-FROM [dbo].[WFSecurities] as Sec
-JOIN [dbo].[DicSecurityLevels] on SEC_LevelID = [TypeID]
-JOIN [dbo].[V_WFElements] on SEC_WFDID = WFD_ID
-WHERE  (SEC_USERGUID = '{User}' or '{User}' in (select u.COS_BpsID from [dbo].[CacheOrganizationStructure] u join [dbo].[CacheOrganizationStructureGroupRelations] gr on gr.COSGR_UserID = u.COS_ID join [dbo].[CacheOrganizationStructure] g on g.COS_ID = gr.COSGR_GroupID where g.COS_DisplayName = SEC_UserName ))
+    [Name] AS [Type of privileges],
+    IIF(SEC_USERGUID = '{User}', 'User', 'Group') AS [User/Group],
+    SEC_Username AS [Name],
+    DEF_Name AS Proces,
+    WF_Name AS Workflow,
+    DTYPE_Name AS DocType,
+    WFD_Signature AS Signature,
+    WFD_ID AS ID
+FROM [dbo].[WFSecurities] AS Sec
+JOIN [dbo].[DicSecurityLevels] 
+    ON SEC_LevelID = [TypeID]
+JOIN [dbo].[V_WFElements] 
+    ON SEC_WFDID = WFD_ID
+WHERE (
+        SEC_USERGUID = '{User}' 
+        OR '{User}' IN (
+            SELECT u.COS_BpsID
+            FROM [dbo].[CacheOrganizationStructure] u
+            JOIN [dbo].[CacheOrganizationStructureGroupRelations] gr
+                ON gr.COSGR_UserID = u.COS_ID
+            JOIN [dbo].[CacheOrganizationStructure] g
+                ON g.COS_ID = gr.COSGR_GroupID
+            WHERE g.COS_DisplayName = SEC_UserName
+        )
+      );
